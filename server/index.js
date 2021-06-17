426af3f81bb6802f677a4924db093bd8296b1802
ferdous85 <ferdous85@gmail.com>
2021-06-17 22:31:50 +0600

    multer added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
 const categoryRoute = require('./routes/categories')
+const multer = require('multer')
+
 
 const app = express()
 
@@ -17,6 +19,21 @@ mongoose.connect(process.env.MONGO_URL,
      useCreateIndex:true,
      useFindAndModify:true
     }).then(console.log('Connected to MongoDB')).catch((err)=>console.log(err));
+
+const storage = multer.diskStorage({
+        destination:  (req, file, cb) => {
+          cb(null, 'image')
+        },
+        filename:  (req, file, cb) => {
+          cb(null, "myimage.jpg")
+        }
+      })
+       
+const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single('file'), (req, res)=>{
+    res.status(200).json("File has been uploaded")
+})
 
 
 
