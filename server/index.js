48422ac9a16552e56cf9273de3a7dc056615bb4d
ferdous85 <ferdous85@gmail.com>
2021-06-17 22:00:31 +0600

    server cng

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
+const postRoute = require('./routes/posts')
+const categoryRoute = require('./routes/categories')
 
 const app = express()
 
@@ -12,13 +14,16 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO_URL, 
     {useNewUrlParser: true,
      useUnifiedTopology: true,
-     useCreateIndex:true
+     useCreateIndex:true,
+     useFindAndModify:true
     }).then(console.log('Connected to MongoDB')).catch((err)=>console.log(err));
 
 
 
 app.use('/api/auth', authRoute )
 app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
+app.use('/api/categories', categoryRoute)
 
 
 app.listen('5000', ()=>{
